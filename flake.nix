64a4860ce1ca3ace02d8a110e50e61f0e2e8b27f
sakif <zizenn@proton.me>
2026-06-16 17:04:40 +0600

    setting up home manager

--- flake.nix
+++ flake.nix
@@ -5,12 +5,11 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05"; # for default nixpkgs
     zen-browser.url = "github:youwen5/zen-browser-flake"; # for zen browser
     lanzaboote.url = "github:nix-community/lanzaboote/v0.4.1"; # for secure boot
-    <home-manager/nixos> # home manager
+    home-manager.url = "github:nix-community/home-manager"; # home manager
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
     nixosConfigurations = {
-      # Change 'nix-port' to your actual hostname (run 'hostname' in terminal to check)
       nix-port = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
 	specialArgs = { inherit inputs; };
