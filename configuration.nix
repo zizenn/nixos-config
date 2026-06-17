0d68b53a65c01a2c6d9b368d6d2d62e77ac74255
sakif <zizenn@proton.me>
2026-06-17 21:19:30 +0600

    fixing some more stuff

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
   boot.loader.efi.canTouchEfiVariables = false;
   boot.kernelPackages = pkgs.linuxPackages;
   boot.kernel.sysctl."kernel.unprivileged_userns_clone" = 1;
+  boot.initrd.availableKernelModules = [ "hv_vmbus" "hv_storvsc" "hv_netvsc" ];
   
   networking = {
     hostName = "nix-port";
