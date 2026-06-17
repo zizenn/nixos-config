810524efa3f8b7b55f4e3b0b987ba74f4e83c0c9
sakif <zizenn@proton.me>
2026-06-17 13:10:37 +0600

    fixing the ur username thing in flake.nix

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.your-username = import ./home.nix;
+            home-manager.users.zizenn = import ./home.nix;
             home-manager.extraSpecialArgs = { inherit inputs; };
           }
         ];
