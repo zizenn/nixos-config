37b9dd2aa496c57b39fd51f670934554263291f0
sakif <zizenn@proton.me>
2026-06-17 13:39:34 +0600

    adding some more programs and linking up my configs stuff

--- home.nix
+++ home.nix
@@ -19,11 +19,26 @@
     git
     neovim
     zsh
+    yazi
+    awww
+    fzf
+    waybar
   ];
 
   programs.git.enable = true;
   programs.zsh.enable = true;
 
-  # Home Manager needs a version
+  # CONFIGURATION
+  xdg.configFile."nvim" = {
+    source = /home/zizenn/dotfiles/nvim;
+    recursive = true; # This ensures all subfolders are linked
+  };
+
+  xdg.configFile."hypr" = {
+    source = /home/zizenn/dotfiles/hyprland;
+    recursive = true;
+  };
+
+  # SYSTEM
   home.stateVersion = "26.05";
 }
