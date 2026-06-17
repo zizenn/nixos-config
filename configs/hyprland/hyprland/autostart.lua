64fd078bc56ebc0a31f8f479c48b55d55e42e65b
sakif <zizenn@proton.me>
2026-06-18 01:24:04 +0600

    refactor: move hyprland configs to separate .lua files

---
+++ configs/hyprland/hyprland/autostart.lua
@@ -0,0 +1,26 @@
+local terminal = "kitty"
+local menu = "wofi -n"
+local bar = "waybar"
+
+hl.on("hyprland.start", function ()
+    -- hyprpolkitagent and pywal
+    hl.exec_cmd("matugen image ~/.wallpaper")
+    hl.exec_cmd("systemctl --user start hyprpolkitagent")
+    hl.exec_cmd("xhost +si:localuser:root")
+
+    -- xdg desktop portal
+    hl.exec_cmd("killall -9 xdg-desktop-portal-hyprland xdg-desktop-portal")
+    hl.exec_cmd("/usr/lib/xdg-desktop-portal-hyprland &")
+    hl.exec_cmd("/usr/lib/xdg-desktop-portal &")
+
+    -- clipboard
+    hl.exec_cmd("wl-paste --type text --watch cliphist store")
+    hl.exec_cmd("wl-paste --type image --watch cliphist store")
+
+    -- wallpaper
+    hl.exec_cmd("awww-daemon")
+
+    -- bar
+    hl.exec_cmd(bar)
+
+end)
---
+++ configs/hyprland/hyprland/window_rules.lua
@@ -0,0 +1,24 @@
+hl.window_rule({
+    match = { class = ".*" },
+    suppress_event = "maximize",
+})
+
+hl.window_rule({
+    match = { class = "^$", title = "^$", xwayland = true, float = true, fullscreen = false, pin = false },
+    no_focus = true,
+})
+
+hl.window_rule({
+    match = { class = "hyprland-run" },
+    move = "20 monitor_h-120",
+    float = true,
+})
+
+hl.window_rule({ match = { initial_class = "^(com.github.martenad.dragon)$" }, float = true, pin = true, size = "200 200", center = true })
+
+hl.window_rule({
+    match = { class = "yazi-float" },
+    float = true,
+    size = "1280 720",
+    center = true,
+})
