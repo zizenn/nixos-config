7137c20ef685abfddae310e5c870c3664e5f3882
sakif <zizenn@proton.me>
2026-06-19 22:43:10 +0600

    remving zen browser from the os

--- configuration.nix
+++ configuration.nix
@@ -102,7 +102,6 @@
       pkgs.waybar
       pkgs.hyprpolkitagent
       pkgs.xdg-desktop-portal-hyprland
-      inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
     ];
     sessionVariables = {
       LIBVA_DRIVER_NAME = "radeonsi";
