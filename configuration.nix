26b36e57123f4797b124d8469d6a3d5c64e6e2da
sakif <zizenn@proton.me>
2026-06-19 23:54:27 +0600

    i forgot wat i did in this configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -59,6 +59,15 @@
     wireplumber.enable = true;
   };
 
+  services.ollama = {
+    enable = true;
+    package = pkgs.ollama-rocm;
+  };
+
+  systemd.services.ollama.environment = {
+    OLLAMA_NUM_GPU_LAYERS = "99";
+  };
+
   time.timeZone = "Asia/Dhaka";
 
   # --- Programs & Environment ---
