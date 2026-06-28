57e33a1c1d90e5a3057cd397d2549b0dfc747775
sakif <zizenn@proton.me>
2026-06-28 11:34:32 +0600

    updating rofi.nix so it actually works

--- configs/rofi.nix
+++ configs/rofi.nix
@@ -7,14 +7,26 @@
 }:
 
 {
-  programs.rofi = {
-    enable = true;
-    package = pkgs.rofi;
+  home.packages = [ pkgs.rofi ];
 
-    extraConfig = {
-      drun-launch = "uwsm app -- {cmd}";
-      run-command = "uwsm app -- {cmd}";
-      run-shell-command = "{terminal} -e uwsm app -- {cmd}";
-    };
+  xdg.configFile."rofi/config.rasi" = {
+    force = true;
+    source = pkgs.writeText "rofi-config.rasi" ''
+      /* ───────────────────────────────────────────────────────────────────
+         「✦ ROFI CONFIG ✦ 」
+         ──────────────────────────────────────────────────────────────── */
+
+      /* ── CONFIGURATION ────────────────────────────────────────────────*/
+      configuration {
+          modi: "drun";
+          show-icons: true;
+          sidebar-mode: false;
+
+          display-drun: "";
+      }
+
+      /* ── THEME ────────────────────────────────────────────────────────*/
+      @theme "~/.config/rofi/themes/glass.rasi"
+    '';
   };
 }
