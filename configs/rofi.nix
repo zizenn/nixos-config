69fb3b6941da1fec7cc78fb5c3c080ca88afe26a
sakif <zizenn@proton.me>
2026-06-30 12:58:43 +0600

    separating my rofi config into separate file

--- configs/rofi.nix
+++ configs/rofi.nix
@@ -5,24 +5,5 @@
 }:
 
 {
-  xdg.configFile."rofi/config.rasi" = {
-    force = true;
-    source = pkgs.writeText "rofi-config.rasi" ''
-      /* ───────────────────────────────────────────────────────────────────
-         「✦ ROFI CONFIG ✦ 」
-         ──────────────────────────────────────────────────────────────── */
-
-      /* ── CONFIGURATION ────────────────────────────────────────────────*/
-      configuration {
-          modi: "drun";
-          show-icons: true;
-          sidebar-mode: false;
-
-          display-drun: "";
-      }
-
-      /* ── THEME ────────────────────────────────────────────────────────*/
-      @theme "~/.config/rofi/themes/glass.rasi"
-    '';
-  };
+  xdg.configFile."rofi/config.rasi".source = ./rofi/config.rasi;
 }
