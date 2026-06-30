55ba3c408f7c13f183d310781bc69459c13f9233
sakif <zizenn@proton.me>
2026-07-01 00:27:28 +0600

    add obsidian.nvim keybindings
    - <leader>oo: open, os: search, od: daily note
    - ob: backlinks, ol: links, ot: toggle checkbox
    - on: new note, or: rename, oq: quick capture

--- configs/nvim/lua/plugins/obsidian.lua
+++ configs/nvim/lua/plugins/obsidian.lua
@@ -4,11 +4,22 @@ return {
     version = "*",
     ft = "markdown",
     dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>oo", "<cmd>ObsidianOpen<cr>", desc = "Open in Obsidian" },
+      { "<leader>os", "<cmd>ObsidianSearch<cr>", desc = "Search Notes" },
+      { "<leader>od", "<cmd>ObsidianToday<cr>", desc = "Daily Note" },
+      { "<leader>ob", "<cmd>ObsidianBacklinks<cr>", desc = "Backlinks" },
+      { "<leader>ol", "<cmd>ObsidianLinks<cr>", desc = "Links" },
+      { "<leader>ot", "<cmd>ObsidianToggleCheckbox<cr>", desc = "Toggle Checkbox" },
+      { "<leader>on", "<cmd>ObsidianNew<cr>", desc = "New Note" },
+      { "<leader>or", "<cmd>ObsidianRename<cr>", desc = "Rename Note" },
+      { "<leader>oq", "<cmd>ObsidianQuickCapture<cr>", desc = "Quick Capture" },
+    },
     opts = {
       workspaces = {
         {
           name = "main",
-          path = "~/path/to/your/vault",  -- update this
+          path = "~/Documents/vault",
         },
       },
     },
