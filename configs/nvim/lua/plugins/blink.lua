094439c615c49d16d546d1011c813e4a9d5d51d8
sakif <zizenn@proton.me>
2026-07-01 22:48:11 +0600

    fix LSP completions: lower min_keyword_length to 0 for immediate trigger on :: . ->

--- configs/nvim/lua/plugins/blink.lua
+++ configs/nvim/lua/plugins/blink.lua
@@ -131,8 +131,8 @@ return {
 			default = { "lsp", "path", "snippets", "buffer" },
 			providers = {
 				lsp = {
-					min_keyword_length = 2, -- Number of characters to trigger porvider
-					score_offset = 0, -- Boost/penalize the score of the items
+					min_keyword_length = 0,
+					score_offset = 0,
 				},
 				path = {
 					min_keyword_length = 0,
