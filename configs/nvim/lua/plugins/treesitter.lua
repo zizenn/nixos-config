0e886728dba9fa60b57f27a41593376a74ac1619
sakif <zizenn@proton.me>
2026-07-01 20:15:30 +0600

    setting up noice.nvim and fixing up treesitter and telescope

--- configs/nvim/lua/plugins/treesitter.lua
+++ configs/nvim/lua/plugins/treesitter.lua
@@ -8,50 +8,25 @@ return {
     },
     cmd = { "TSUpdateSync", "TSInstall", "TSUninstall" },
     opts = {
-      -- A list of parser names, or "all"
       ensure_installed = {
         "bash", "c", "lua", "vim", "vimdoc", "query",
         "javascript", "typescript", "tsx", "html", "css", 
         "json", "yaml", "markdown", "markdown_inline",
       },
-
-      -- Install parsers synchronously (only applied to `ensure_installed`)
       sync_install = false,
-
-      -- Automatically install missing parsers when entering buffer
       auto_install = true,
-
       highlight = {
         enable = true,
-        -- Setting this to true will run `:h syntax` and may slow down your editor
-        -- for large files. Set to `false` if you experience lag.
         additional_vim_regex_highlighting = false,
       },
-
       indent = { enable = true },
-
-      -- nvim-treesitter-textobjects configuration
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true, -- Automatically jump forward to textobj
-          keymaps = {
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-            ["as"] = "@scope",
-          },
-        },
-      },
     },
     config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
+      require("nvim-treesitter.config").setup(opts)
 
-      -- Enable Treesitter based folding
       vim.opt.foldmethod = "expr"
       vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-      vim.opt.foldenable = false -- Disable folding by default at startup
+      vim.opt.foldenable = false 
     end,
   },
 }
