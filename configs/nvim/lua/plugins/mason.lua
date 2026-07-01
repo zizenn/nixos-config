e5e22675b05bf86c22c044f75cfe33842924e639
sakif <zizenn@proton.me>
2026-07-01 22:17:58 +0600

    removing that bum ahh tsserver in mason

--- configs/nvim/lua/plugins/mason.lua
+++ configs/nvim/lua/plugins/mason.lua
@@ -1,45 +1,54 @@
 return {
-  -- Main mason plugin
-  {
-    "williamboman/mason.nvim",
-    lazy = false, 
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
-    },
-    config = function()
-      local mason = require("mason")
-      local mason_lspconfig = require("mason-lspconfig")
-      local mason_tool_installer = require("mason-tool-installer")
+	-- Main mason plugin
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+		},
+		config = function()
+			local mason = require("mason")
+			local mason_lspconfig = require("mason-lspconfig")
+			local mason_tool_installer = require("mason-tool-installer")
 
-      mason.setup({
-        ui = {
-          border = "rounded",
-          icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗",
-          },
-        },
-      })
+			mason.setup({
+				ui = {
+					border = "rounded",
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			})
 
-      mason_lspconfig.setup({
-        ensure_installed = {
-          "lua_ls", "tsserver", "html", "cssls", "pyright", "clangd",
-        },
-        automatic_installation = true,
-      })
+			mason_lspconfig.setup({
+				ensure_installed = {
+					"lua_ls",
+					"html",
+					"cssls",
+					"pyright",
+					"clangd",
+				},
+				automatic_installation = true,
+			})
 
-      mason_tool_installer.setup({
-        ensure_installed = {
-          "stylua", "prettier", "eslint_d", "black", "debugpy", "clang-format", "codelldb",
-        },
-        auto_update = true,
-        run_on_start = true,
-      })
+			mason_tool_installer.setup({
+				ensure_installed = {
+					"stylua",
+					"prettier",
+					"eslint_d",
+					"black",
+					"debugpy",
+					"clang-format",
+					"codelldb",
+				},
+				auto_update = true,
+				run_on_start = true,
+			})
 
-      vim.cmd("MasonToolsInstall") 
-    end,
-  },
+			vim.cmd("MasonToolsInstall")
+		end,
+	},
 }
-
