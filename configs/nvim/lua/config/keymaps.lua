e2199f4055190c3aadba0c880dc3ab391d379319
sakif <zizenn@proton.me>
2026-07-01 20:06:47 +0600

    adding treesitter and fixing telescope keymaps

--- configs/nvim/lua/config/keymaps.lua
+++ configs/nvim/lua/config/keymaps.lua
@@ -8,9 +8,8 @@ vim.g.maplocalleader = " "
 map("n", "<leader>e", "<cmd>Neotree toggle<cr>", { desc = "Toggle Neo-tree" })
 
 -- telescope
-map("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Telescope Find Files" })
-map("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { desc = "Telescope Live Grep" })
-map("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "Telescope Find Buffers" })
-map("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "Telescope Help Tags" })
-map("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>", { desc = "Telescope Recent Files" })
-
+map("n", "<leader>ff", function() require("telescope.builtin").find_files() end, { desc = "Telescope Find Files" })
+map("n", "<leader>fg", function() require("telescope.builtin").live_grep() end, { desc = "Telescope Live Grep" })
+map("n", "<leader>fb", function() require("telescope.builtin").buffers() end, { desc = "Telescope Find Buffers" })
+map("n", "<leader>fh", function() require("telescope.builtin").help_tags() end, { desc = "Telescope Help Tags" })
+map("n", "<leader>fo", function() require("telescope.builtin").oldfiles() end, { desc = "Telescope Recent Files" })
