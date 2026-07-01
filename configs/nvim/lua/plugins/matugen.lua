9654dd8331f614ab2c4e8d1fe27dde0c4ebe1751
sakif <zizenn@proton.me>
2026-07-01 23:19:21 +0600

    fix matugen theme path: ~/.cache instead of ~/.config

--- configs/nvim/lua/plugins/matugen.lua
+++ configs/nvim/lua/plugins/matugen.lua
@@ -5,17 +5,7 @@ return {
   opts = {
     load_theme = true,
 
-    jsonc_path = vim.fn.expand("~/.config/matugen/themes/nvim-colors.jsonc"),
+    jsonc_path = vim.fn.expand("~/.cache/matugen/themes/nvim-colors.jsonc"),
   },
-  config = function(_, opts)
-    require("matugen").setup(opts)
-    vim.api.nvim_create_autocmd("ColorScheme", {
-      group = vim.api.nvim_create_augroup("matugen_fix", { clear = true }),
-      callback = function()
-        if pcall(require, "nvim-treesitter") then
-          vim.cmd("TSBufEnable highlight")
-        end
-      end,
-    })
-  end,
+
 }
