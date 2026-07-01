f0a9a13c743b2850cca3cd9c94d36daa05a0b11b
sakif <zizenn@proton.me>
2026-07-01 23:16:38 +0600

    doing a thing

--- configs/matugen.nix
+++ configs/matugen.nix
@@ -4,11 +4,10 @@
   xdg.configFile = {
     "matugen/config.toml".source = ./matugen/config.toml;
 
-    "matugen/templates/nvim.jsonc".source = ./matugen/templates/nvim.jsonc;
+    "matugen/templates/nvim-colors.jsonc".source = ./matugen/templates/nvim-colors.jsonc;
     "matugen/templates/rofi-colors.rasi".source = ./matugen/templates/rofi.rasi;
     "matugen/templates/zed-theme.json".source = ./matugen/templates/zed.json;
     "matugen/templates/kitty.conf".source = ./matugen/templates/kitty.conf;
-    "matugen/templates/waylandar-theme.qml".source = ./matugen/templates/waylandar.qml;
     "matugen/templates/waybar.css".source = ./matugen/templates/waybar.css;
     "matugen/templates/obsidian.css".source = ./matugen/templates/obsidian.css;
     "matugen/templates/vesktop.css".source = ./matugen/templates/vesktop.css;
