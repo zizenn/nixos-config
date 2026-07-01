58a3c615d3256a802851f03f176db8fb3d7e6f11
sakif <zizenn@proton.me>
2026-07-01 22:36:27 +0600

    lazy-load blink.cmp on file entry

--- configs/nvim/lua/plugins/blink.lua
+++ configs/nvim/lua/plugins/blink.lua
@@ -1,59 +1,144 @@
 return {
-	{
-		"saghen/blink.cmp",
-		lazy = false,
-		dependencies = {
-			"rafamadriz/friendly-snippets",
+	"saghen/blink.cmp",
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+		"onsails/lspkind.nvim",
+	},
+	version = "*",
+	event = { "BufReadPre", "BufNewFile" },
+
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
+	opts = {
+
+		appearance = {
+			use_nvim_cmp_as_default = false,
+			nerd_font_variant = "mono",
 		},
-		version = "*",
 
-		opts = {
-			keymap = {
-				preset = "none",
+		completion = {
+			accept = { auto_brackets = { enabled = true } },
 
-				["<CR>"] = { "accept", "fallback" },
-
-				["<Tab>"] = {
-					"snippet_forward",
-					"select_next",
-					"fallback",
-				},
-
-				["<S-Tab>"] = {
-					"snippet_backward",
-					"select_prev",
-					"fallback",
-				},
-
-				["<C-space>"] = { "show", "hide" },
-				["<C-e>"] = { "hide" },
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 250,
+				treesitter_highlighting = true,
+				window = { border = "rounded" },
 			},
 
-			completion = {
-				min_keyword_length = 2,
-
-				keyword = {
-					delay = 250, -- Keeps your custom 250ms delay intact
-				},
-
-				accept = { auto_brackets = { enabled = true } },
-
-				-- 📐 Lock the visual popups positioning behavior
-				menu = {
-					direction_priority = { "s", "n" }, -- Forces South (below), South-East, South-West directions
-					draw = { treesitter = { "lsp" } },
-				},
+			list = {
+				selection = function(ctx)
+					return ctx.mode == "cmdline" and "auto_insert" or "preselect"
+				end,
 			},
 
-			appearance = {
-				use_nvim_cmp_as_default = true,
-				nerd_font_variant = "mono",
-			},
+			menu = {
+				border = "rounded",
 
-			sources = {
-				default = { "lsp", "path", "snippets", "buffer" },
+				cmdline_position = function()
+					if vim.g.ui_cmdline_pos ~= nil then
+						local pos = vim.g.ui_cmdline_pos -- (1, 0)-indexed
+						return { pos[1] - 1, pos[2] }
+					end
+					local height = (vim.o.cmdheight == 0) and 1 or vim.o.cmdheight
+					return { vim.o.lines - height, 0 }
+				end,
+
+				draw = {
+					columns = {
+						{ "kind_icon", "label", gap = 1 },
+						{ "kind" },
+					},
+					components = {
+						kind_icon = {
+							text = function(item)
+								local kind = require("lspkind").symbol_map[item.kind] or ""
+								return kind .. " "
+							end,
+							highlight = "CmpItemKind",
+						},
+						label = {
+							text = function(item)
+								return item.label
+							end,
+							highlight = "CmpItemAbbr",
+						},
+						kind = {
+							text = function(item)
+								return item.kind
+							end,
+							highlight = "CmpItemKind",
+						},
+					},
+				},
 			},
 		},
-		opts_extend = { "sources.default" },
+
+		-- My super-TAB configuration
+		keymap = {
+			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+			["<C-e>"] = { "hide", "fallback" },
+			["<CR>"] = { "accept", "fallback" },
+
+			["<Tab>"] = {
+				function(cmp)
+					return cmp.select_next()
+				end,
+				"snippet_forward",
+				"fallback",
+			},
+			["<S-Tab>"] = {
+				function(cmp)
+					return cmp.select_prev()
+				end,
+				"snippet_backward",
+				"fallback",
+			},
+
+			["<Up>"] = { "select_prev", "fallback" },
+			["<Down>"] = { "select_next", "fallback" },
+			["<C-p>"] = { "select_prev", "fallback" },
+			["<C-n>"] = { "select_next", "fallback" },
+			["<C-up>"] = { "scroll_documentation_up", "fallback" },
+			["<C-down>"] = { "scroll_documentation_down", "fallback" },
+		},
+
+		-- Experimental signature help support
+		signature = {
+			enabled = true,
+			window = { border = "rounded" },
+		},
+
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+			cmdline = function()
+				local type = vim.fn.getcmdtype()
+				-- Search forward and backward
+				if type == "/" or type == "?" then
+					return { "buffer" }
+				end
+				-- Commands
+				if type == ":" then
+					return { "cmdline" }
+				end
+				return {}
+			end,
+			providers = {
+				lsp = {
+					min_keyword_length = 2, -- Number of characters to trigger porvider
+					score_offset = 0, -- Boost/penalize the score of the items
+				},
+				path = {
+					min_keyword_length = 0,
+				},
+				snippets = {
+					min_keyword_length = 2,
+				},
+				buffer = {
+					min_keyword_length = 5,
+					max_items = 5,
+				},
+			},
+		},
 	},
 }
