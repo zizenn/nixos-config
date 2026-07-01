57346cf871f39ba9384a06219cbd75b165e896b8
sakif <zizenn@proton.me>
2026-07-01 23:47:04 +0600

    fix lsp: use vim.lsp.config API + fix clangd --function-arg-placeholders flag

--- configs/nvim/lua/plugins/lsp.lua
+++ configs/nvim/lua/plugins/lsp.lua
@@ -1,41 +1,32 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = { "saghen/blink.cmp" },
-		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-			local lspconfig = require("lspconfig")
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "saghen/blink.cmp" },
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-			local capabilities = require("blink.cmp").get_lsp_capabilities()
+      local servers = { "lua_ls", "ts_ls", "html", "cssls", "pyright" }
 
-			local servers = { "lua_ls", "ts_ls", "html", "cssls", "pyright" }
+      for _, server in ipairs(servers) do
+        vim.lsp.config(server, {
+          capabilities = capabilities,
+        })
+        vim.lsp.enable(server)
+      end
 
-			for _, server in ipairs(servers) do
-				lspconfig[server].setup({
-					capabilities = capabilities,
-				})
-			end
-
-			local nix_clangd = vim.fn.expand("~/.nix-profile/bin/clangd")
-			local clangd_cmd = vim.fn.executable(nix_clangd) == 1
-				and { nix_clangd }
-				or { "clangd" }
-
-			lspconfig.clangd.setup({
-				capabilities = capabilities,
-				cmd = vim.list_extend(clangd_cmd, {
-					"--background-index",
-					"--clang-tidy",
-					"--header-insertion=iwyu",
-					"--completion-style=detailed",
-					"--function-arg-placeholders",
-				}),
-				init_options = {
-					usePlaceholders = true,
-					completeUnimported = true,
-					clangdFileStatus = true,
-				},
-			})
-		end,
-	},
+      vim.lsp.config("clangd", {
+        capabilities = capabilities,
+        cmd = {
+          "clangd",
+          "--background-index",
+          "--clang-tidy",
+          "--header-insertion=iwyu",
+          "--completion-style=detailed",
+          "--function-arg-placeholders=true",
+        },
+      })
+      vim.lsp.enable("clangd")
+    end,
+  },
 }
