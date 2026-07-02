bb5f51a34448c7708358b6ebdc15cd954e9ed6f6
sakif <zizenn@proton.me>
2026-07-02 20:47:52 +0600

    rofi: add clipboard history manager with image support via cliphist
    - cliphist-rofi-img script for browsing clipboard history in rofi
    - clipboard.rasi squared-off theme following new design language
    - Super+V keybinding already wired in hyprland config

--- configs/rofi.nix
+++ configs/rofi.nix
@@ -6,4 +6,10 @@
 
 {
   xdg.configFile."rofi/config.rasi".source = ./rofi/config.rasi;
+  xdg.configFile."rofi/clipboard.rasi".source = ./rofi/clipboard.rasi;
+
+  home.file.".local/bin/cliphist-rofi-img" = {
+    source = ./rofi/cliphist-rofi-img;
+    executable = true;
+  };
 }
