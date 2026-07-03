7d4316deba6994256f4917c8e528c48d5d57b01d
sakif <zizenn@proton.me>
2026-07-03 14:39:34 +0600

    adding S as a bind for surround nvim

--- configs/nvim/lua/plugins/surround.lua
+++ configs/nvim/lua/plugins/surround.lua
@@ -1,8 +1,14 @@
 return {
-  {
-    "kylechui/nvim-surround",
-    version = "*",
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {},
-  },
+	{
+		"kylechui/nvim-surround",
+		version = "*",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("nvim-surround").setup({
+				keymaps = {
+					visual = "S", -- Forces Shift+S in visual mode
+				},
+			})
+		end,
+	},
 }
