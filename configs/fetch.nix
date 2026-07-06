ceef402845b3049e3cebf9634b400205d2872c5b
sakif <zizenn@proton.me>
2026-07-06 12:49:47 +0600

    updating the fetch config to add more stuf

--- configs/fetch.nix
+++ configs/fetch.nix
@@ -1,12 +1,19 @@
 { config, pkgs, ... }: {
   programs.fetch = {
     enable = true;
-    labelColor = "red";
+    labelColor = "blue";
     info = [
       "os"
       "host"
       "kernel"
       "uptime"
+      "shell"
+      "wm"
+      "terminal"
+      "cpu"
+      "gpu"
+      "ip"
+      "colors"
     ];
     speed = 1.0;
     spin = "xy";
