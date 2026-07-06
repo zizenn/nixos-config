83d6474442550d6759a2662f5e099ac1da426d61
sakif <zizenn@proton.me>
2026-07-06 12:39:42 +0600

    clean up flake.nix: consistent input style, remove unnecessary destructuring

--- flake.nix
+++ flake.nix
@@ -1,55 +1,40 @@
 {
-  description = "my nixos flake";
+  description = "zizenn's NixOS configuration";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     zen-browser = {
       url = "github:youwen5/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    home-manager.url = "github:nix-community/home-manager/release-26.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    areofyl-fetch.url = "github:areofyl/fetch";
     herdr.url = "github:ogulcancelik/herdr";
     wlctl.url = "github:aashish-thapa/wlctl";
-    areofyl-fetch.url = "github:areofyl/fetch";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      home-manager,
-      zen-browser,
-      herdr,
-      areofyl-fetch,
-      ...
-    }@inputs:
-    {
-      nixosConfigurations = {
-        nix-port = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          specialArgs = { inherit inputs; };
-          modules = [
-            ./configuration.nix
-          ];
-        };
-      };
-
-      # This block enables "nh home switch" to work
-      homeConfigurations = {
-        "zizenn@nix-port" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          extraSpecialArgs = { inherit inputs; };
-          modules = [
-            ./home.nix
-            inputs.areofyl-fetch.homeManagerModules.default
-            {
-              home.username = "zizenn";
-              home.homeDirectory = "/home/zizenn";
-              home.stateVersion = "26.05";
-            }
-          ];
-        };
-      };
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.nix-port = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [ ./configuration.nix ];
     };
+
+    homeConfigurations."zizenn@nix-port" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      extraSpecialArgs = { inherit inputs; };
+      modules = [
+        ./home.nix
+        inputs.areofyl-fetch.homeManagerModules.default
+        {
+          home.username = "zizenn";
+          home.homeDirectory = "/home/zizenn";
+          home.stateVersion = "26.05";
+        }
+      ];
+    };
+  };
 }
