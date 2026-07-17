ff6f57a303b4e18d231bdeffc3fba9d4821c72a4
sakif <zizenn@proton.me>
2026-07-17 21:30:06 +0600

    fixing man thing

--- configs/shell.nix
+++ configs/shell.nix
@@ -37,7 +37,8 @@
       set -gx EDITOR "nvim"
       set -gx SUDO_EDITOR "nvim"
       set -gx VISUAL "nvim"
-      set -gx MANPAGER "bat -plman"
+      set -gx MANROFFOPT "-c"
+      set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
       set -gx FZF_DEFAULT_COMMAND "fd --type f --strip-cwd-prefix --hidden --follow --exclude .git"
 
       set -U fish_greeting ""
