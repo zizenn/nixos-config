abc3a97f39f66c30cb1500cbdfdc8383a9e1de6f
sakif <zizenn@proton.me>
2026-07-22 23:43:07 +0600

    niri: set 260Hz, split config into fragments, strip boilerplate comments

--- modules/home/desktop/niri/default.nix
+++ modules/home/desktop/niri/default.nix
@@ -1,8 +1,19 @@
 { config, pkgs, ... }:
 
+let
+  fragments = [
+    ./config/01-input.kdl
+    ./config/02-outputs.kdl
+    ./config/03-layout.kdl
+    ./config/04-main.kdl
+    ./config/05-window-rules.kdl
+    ./config/06-binds.kdl
+  ];
+  configText = builtins.concatStringsSep "\n" (map builtins.readFile fragments);
+in
 {
   xdg.configFile = {
-    "niri/config.kdl".source = ./config.kdl;
+    "niri/config.kdl".text = configText;
     "hypr/hypridle.conf".source = ./hypridle.conf;
   };
 }
