7511541cf42798f856b6981443a61806a8bd121d
zizenn-pc <zizenn.69@gmail.com>
2026-07-27 23:40:54 +0930

    starship: follow matugen/kanagawa-dragon colors
    - Add matugen template starship.toml generating ~/.config/starship/matugen.toml
    - Add kanagawa-dragon pre-rendered starship.toml for static fallback
    - Register template in matugen.nix and config.toml
    - Register kanagawa-dragon file in kanagawa-dragon.nix
    - Add copy line to theme-kanagawa script
    - Remove static starship.settings from shell.nix, set STARSHIP_CONFIG env

--- modules/shell.nix
+++ modules/shell.nix
@@ -28,6 +28,7 @@
         sudo = "doas";
       };
       interactiveShellInit = ''
+        set -gx STARSHIP_CONFIG ~/.config/starship/matugen.toml
         set -gx MANROFFOPT "-c"
         set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
         set -gx FZF_DEFAULT_COMMAND "fd --type f --strip-cwd-prefix --hidden --follow --exclude .git"
@@ -41,35 +42,6 @@
       enable = true;
       enableFishIntegration = true;
       enableTransience = true;
-      settings = {
-        scan_timeout = 50;
-        command_timeout = 2000;
-        format = "$directory$git_branch$git_status$fill${"\n"}$character";
-        fill.symbol = " ";
-        continuation_prompt = "[▸▹ ](dimmed white)";
-        directory = {
-          style = "bold blue";
-          format = "[$path]($style)[$read_only]($read_only_style) ";
-          truncate_to_repo = true;
-        };
-        character = {
-          success_symbol = "[❯](purple bold)";
-          error_symbol = "[❯](red bold)";
-          vimcmd_symbol = "[❮](green bold)";
-        };
-        git_branch = {
-          format = "[△ $branch]($style) ";
-          style = "italic bright-blue";
-        };
-        git_status = {
-          format = "[$all_status$ahead_behind]($style) ";
-          style = "bold bright-black";
-        };
-        cmd_duration = {
-          format = "[$duration]($style)";
-          style = "italic dimmed yellow";
-        };
-      };
     };
     home.packages = with pkgs; [
       bat bc broot btop catimg cava chafa cmatrix
