e223272302c5b13edaec36c91e26f0df95bca242
zizenn-pc <zizenn.69@gmail.com>
2026-08-03 23:04:18 +0930

    adding wake on lan and renaming nix port to host

--- modules/networking.nix
+++ modules/networking.nix
@@ -1,13 +1,22 @@
-{lib, ...}: {
+{ lib, ... }: {
   nixos.modules.base = {
     networking = {
       hostName = "zizenn-hack";
       networkmanager.enable = true;
       networkmanager.dns = "none";
-      nameservers = ["8.8.8.8" "1.1.1.1"];
+      nameservers = [
+        "8.8.8.8"
+        "1.1.1.1"
+      ];
       enableIPv6 = true;
       interfaces.wlo1.mtu = 1400;
-      firewall.allowedTCPPorts = [8080 8081];
+      firewall.allowedTCPPorts = [
+        8080
+        8081
+      ];
+
+      # wake on LAN
+      interfaces.enp8s0.wakeOnLan.enable = true;
     };
   };
 }
